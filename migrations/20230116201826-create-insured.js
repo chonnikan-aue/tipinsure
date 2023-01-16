fedc8ad65eede6c4ee0e5cb543c2d758051604e3
Chonnikan <chonnikan.aue@gmail.com>
2023-01-17 03:30:24 +0700

    add Insured model

---
+++ migrations/20230116201826-create-insured.js
@@ -0,0 +1,139 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Insureds", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+      },
+      policyNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      idCardOrPassportNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      dateOfBirth: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      tel: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      addressHouseNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      addressVillageNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      addressSubDistrict: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      addressDistrict: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      addressProvince: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      addressZipCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      beneficiaryRelationshipName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      beneficiaryTitle: {
+        type: Sequelize.STRING,
+      },
+      beneficiaryFirstName: {
+        type: Sequelize.STRING,
+      },
+      beneficiaryLastName: {
+        type: Sequelize.STRING,
+      },
+      healthQuestion1: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      healthQuestion2: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      healthQuestion3: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      taxpayerNumber: {
+        type: Sequelize.STRING,
+      },
+      deliveryAddressHouseNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deliveryAddressVillageNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deliveryAddressSubDistrict: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deliveryAddressDistrict: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deliveryAddressProvince: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deliveryAddressZipCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Insureds");
+  },
+};
---
+++ models/insured.js
@@ -0,0 +1,51 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Insured extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Insured.belongsTo(models.User, { foreignKey: "userId" });
+    }
+  }
+  Insured.init({
+    userId: DataTypes.INTEGER,
+    policyNumber: DataTypes.STRING,
+    title: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    idCardOrPassportNumber: DataTypes.STRING,
+    dateOfBirth: DataTypes.DATEONLY,
+    tel: DataTypes.STRING,
+    addressHouseNumber: DataTypes.STRING,
+    addressVillageNumber: DataTypes.STRING,
+    addressSubDistrict: DataTypes.STRING,
+    addressDistrict: DataTypes.STRING,
+    addressProvince: DataTypes.STRING,
+    addressZipCode: DataTypes.STRING,
+    email: DataTypes.STRING,
+    beneficiaryRelationshipName: DataTypes.STRING,
+    beneficiaryTitle: DataTypes.STRING,
+    beneficiaryFirstName: DataTypes.STRING,
+    beneficiaryLastName: DataTypes.STRING,
+    healthQuestion1: DataTypes.STRING,
+    healthQuestion2: DataTypes.STRING,
+    healthQuestion3: DataTypes.STRING,
+    taxpayerNumber: DataTypes.STRING,
+    deliveryAddressHouseNumber: DataTypes.STRING,
+    deliveryAddressVillageNumber: DataTypes.STRING,
+    deliveryAddressSubDistrict: DataTypes.STRING,
+    deliveryAddressDistrict: DataTypes.STRING,
+    deliveryAddressProvince: DataTypes.STRING,
+    deliveryAddressZipCode: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Insured',
+  });
+  return Insured;
+};
